4f1c9968d18b28eb7e9000a917f20423e3196a95
Ali Arslan <aliarslanmushtaqali@gmail.com>
2023-08-01 20:30:23 +0500

    Updated secondary location

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ module "primary-app-backend" {
 
 #provision SQL Database for the data storage in Primary Region
 module "primary-sql" {
-  source        = "./modules/storage/azure_sql"
+  source        = "./modules/database/azure_sql"
   rgname        = module.primary-rg.resource_group_name
   location      = "eastus"
   sqlservername = "primarysqlserverdr"
@@ -61,14 +61,14 @@ module "primary-sql" {
 module "secondary-rg" {
   source   = "./modules/management/azure_resource_group"
   rgname   = "rg-eus-dr"
-  location = "eastus"
+  location = "westeurope"
 }
 
 #provision ASP in Secondary Region
 module "secondary-asp-plan" {
   source   = "./modules/webapps/azure_app_service_plan"
   rgname   = module.secondary-rg.resource_group_name
-  location = "eastus"
+  location = "westeurope"
   aspname  = "eus-asp-dr"
   skusize  = "S1"
   os_type  = "Linux"
@@ -78,7 +78,7 @@ module "secondary-asp-plan" {
 module "secondary-app-frontend" {
   source   = "./modules/webapps/azure_app_service"
   rgname   = module.secondary-rg.resource_group_name
-  location = "eastus"
+  location = "westeurope"
   appname  = "eus-frontend-app-dr"
   aspid    = module.secondary-asp-plan.asp_id
 }
@@ -87,16 +87,16 @@ module "secondary-app-frontend" {
 module "secondary-app-backend" {
   source   = "./modules/webapps/azure_app_service"
   rgname   = module.secondary-rg.resource_group_name
-  location = "eastus"
+  location = "westeurope"
   appname  = "eus-backend-app-dr"
   aspid    = module.secondary-asp-plan.asp_id
 }
 
 #provision SQL Database for the data storage in Secondary Region
 module "secondary-sql" {
-  source        = "./modules/storage/azure_sql"
+  source        = "./modules/database/azure_sql"
   rgname        = module.secondary-rg.resource_group_name
-  location      = "eastus"
+  location      = "westeurope"
   sqlservername = "secondarysqlserverdr"
   dbname        = "secondarydb"
   dbsize        = "1024"
@@ -110,12 +110,22 @@ module "secondary-sql" {
 ################################################################################################  
 #                                       Traffic Manager                                        #
 ################################################################################################  
-module "tm" {
+module "global-tm" {
   source = "./modules/networking/azure_traffic_manager"
   rgname = module.primary-rg.resource_group_name
   name   = "global-trafficmanager-dr"
   primary_hostname = module.primary-app-frontend.asp_hostname
   secondary_hostname = module.secondary-app-frontend.asp_hostname
   primary_location = "eastus"
-  secondary_location = "westus"
+  secondary_location = "westeurope"
+}
+################################################################################################  
+#                                       SQL Geo-Replication                                    #
+################################################################################################  
+module "geo-Replication" {
+  source = "./modules/database/azure_sql_failover"
+  primary_db_id = module.primary-sql.db_id
+  secondary_db_id = module.secondary-sql.db_id
+  primary_server_id = module.primary-sql.server_id
+  secondary_server_id = module.secondary-sql.server_id
 }
