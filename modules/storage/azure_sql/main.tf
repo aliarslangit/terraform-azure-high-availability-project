b35f28a1a0b48b28f32f8db570f4b085439ebc13
Ali Arslan <aliarslanmushtaqali@gmail.com>
2023-08-01 19:55:44 +0500

    updated naming convention

--- modules/storage/azure_sql/main.tf
+++ modules/storage/azure_sql/main.tf
@@ -1,17 +1,17 @@
-resource "azurerm_sql_server" "demo" {
+resource "azurerm_sql_server" "sql" {
   name                         = var.sqlservername
-  resource_group_name          = azurerm_resource_group.demo.name
-  location                     = azurerm_resource_group.demo.location
-  version                      = var.version
+  resource_group_name          = var.rgname
+  location                     = var.location
+  version                      = var.db_version
   administrator_login          = var.username
   administrator_login_password = var.password
   tags = {
     Environment = var.Environment
   }
 }
-resource "azurerm_mssql_database" "demo" {
+resource "azurerm_mssql_database" "sql" {
   name           = var.dbname
-  server_id      = azurerm_sql_server.demo.id
+  server_id      = azurerm_sql_server.sql.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
   max_size_gb    = var.dbsize
